93eabd873742429e7eb8e159e02a8febfc708b78
Dvd <everythingexplained1234@gmail.com>
2022-02-10 07:00:27 -0500

    Added code to interact with sqlite db

--- backend/main.py
+++ backend/main.py
@@ -2,11 +2,67 @@ from AnilistPython import Anilist
 from pprint import pprint
 import pyperclip
 
+import sqlite3
+from sqlite3 import Error
+
 from wrappers import *
-from test import test
+
+
+def create_connection(database):
+    conn = None
+    try:
+        conn = sqlite3.connect(database)
+        return conn
+    except Error as e:
+        raise e
+
+    raise Exception()
+
+
+def execute_sql(sql):
+    conn = create_connection("./database/anime.db")
+    try:
+        c = conn.cursor()
+        c.execute(sql)
+        conn.commit()
+        rows = c.fetchall()
+
+        for r in rows:
+            print(r)
+    except Error as e:
+        raise e
+
+
+def setup_table():
+    sql = """
+        CREATE TABLE IF NOT EXISTS anime (
+            id INTEGER PRIMARY KEY,
+            anime_id INTEGER,
+            stream_service VARCHAR
+        );
+    """
+    execute_sql(sql)
+
 
 anilist = Anilist()
+setup_table()
 
+anime_name = input("What anime would you like to see? ")
+anime_info = anilist.extractID.anime(anime_name)["data"]["Page"]["media"]
+# print(anime_info)
+for i in range(len(anime_info)):
+    anime = anime_info[i]
+    # pprint(anime)
+    title = anime["title"]["english"] if anime["title"]["english"] else anime["title"]["romaji"]
+    print(f"{i + 1}. {title}")
 
-pprint(anilist.extractID.anime("Code Geass"))
-pyperclip.copy(str(anilist.extractID.anime("Code Geass")))
+choice = input("Which one of these is correct? ")
+anime_id = anime_info[int(choice) - 1]["id"]
+sql = f"""
+INSERT INTO anime(anime_id) VALUES({anime_id});
+"""
+execute_sql(sql)
+
+# pprint(anilist.extractInfo.anime(anime_id))
+
+# pyperclip.copy(str(anime_info))
--- backend/test.py
+++ backend/test.py
@@ -1,11 +1,39 @@
 from AnilistPython import Anilist
 from pprint import pprint
 
+import sqlite3
+from sqlite3 import Error
+
 from wrappers import *
 
-anilist = Anilist()
 
-def test():
-    id = anilist.extractID.anime("Code Geass")["id"]
-    pprint(anilist.extractInfo.anime(id))
-    # pprint(anilist.getAnimeInfo("Code Geass"))
+def create_connection(database):
+    conn = None
+    try:
+        conn = sqlite3.connect(database)
+        return conn
+    except Error as e:
+        print(e)
+
+    raise Exception()
+
+
+def execute_sql(sql):
+    conn = create_connection("./database/anime.db")
+    try:
+        c = conn.cursor()
+        c.execute(sql)
+        conn.commit()
+        rows = c.fetchall()
+
+        for r in rows:
+            print(r)
+    except Error as e:
+        print(e)
+
+if __name__ == "__main__":
+    print("ok")
+    sql = """
+    SELECT * FROM test
+    """
+    execute_sql(sql)
